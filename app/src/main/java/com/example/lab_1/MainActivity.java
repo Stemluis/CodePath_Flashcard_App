f3544a9210f8c275c791f3dd9832aca2d8e01d1f
Stemluis <stemluis@gmail.com>
2021-02-23 19:52:03 -0500

    Completed required tasks

--- app/src/main/java/com/example/lab_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView question = findViewById(R.id.flashcard_question);
+        TextView answer = findViewById(R.id.flashcard_answer);
+
+        question.setOnClickListener(v -> {
+            question.setVisibility(View.GONE);
+            answer.setVisibility(View.VISIBLE);
+        });
+
+        answer.setOnClickListener(v -> {
+            answer.setVisibility(View.GONE);
+            question.setVisibility(View.VISIBLE);
+        });
     }
 }
